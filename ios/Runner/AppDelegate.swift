16144d99a87c38df78701d5e28f6d8acac6e0ad1
Alaa Alzibda <alaa07996@gmail.com>
2022-06-26 12:02:47 +0300

    remove key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDR5axoEe1xkZuG-oDusvkLguyNrTvWedI")
+    // GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
